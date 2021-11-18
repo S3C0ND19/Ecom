5e2d58bf9698396502a58f8a399b77c557f6613a
S3C0ND19 <90059594+S3C0ND19@users.noreply.github.com>
2021-11-18 22:01:17 +0700

    up

---
+++ .history/includes/footer_20211118193906.php
@@ -0,0 +1,228 @@
+<div class="footer__wrapper">
+          <div class="social">
+            <div class="social__container">
+              <div class="social__img">
+                <img src="./assets/img/logo_social.png" alt="">
+              </div>
+              <div class="social__btn">
+                <ul class="social__btn-item">
+                  <li class="social__btn-list">
+                    <a href="" class="social_btn-link">
+                      <i class="social__btn-icon ti-twitter-alt"></i>
+                    </a>
+                  </li>
+                  <li class="social__btn-list">
+                    <a href="" class="social_btn-link">
+                      <i class="social__btn-icon ti-instagram"></i>
+                    </a>
+                  </li>
+                  <li class="social__btn-list">
+                    <a href="" class="social_btn-link">
+                      <i class="social__btn-icon ti-facebook"></i>
+                    </a>
+                  </li>
+                  <li class="social__btn-list">
+                    <a href="" class="social_btn-link">
+                      <i class="social__btn-icon ti-youtube"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <footer class="footer">
+            <div class="footer__container">
+              <div class="grid">
+                <div class="row">
+                  <div class="col l-3">
+                    <span class="footer__heading">GET HELP</span>
+                    <ul class="footer__container-item">
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Contact & FAQ
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Track Your Order
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Shipping & Delivery
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Visit Brisbane Studio
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Interest Free Finance
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="col l-3">
+                    <span class="footer__heading">SERVICES</span>
+                    <ul class="footer__container-item">
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Assembly Guides
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Furniture Packages & Fitouts
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Trade Programmer
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Sale
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          New Designs
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="col l-3">
+                    <span class="footer__heading">CONNECT</span>
+                    <ul class="footer__container-item">
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Twiter
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Facebook
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Instagram
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Pinterest
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Jobs
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="col l-3">
+                    <span class="footer__heading">CATEGORIES</span>
+                    <ul class="footer__container-item">
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Armchairs
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Bath Room
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Dining Chairs
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Dining Tables
+                        </a>
+                      </li>
+                      <li class="footer__container-list">
+                        <a href="" class="footer__container-link">
+                          Living Room
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>         
+            </div>
+          </footer>
+          <!-- copyright -->
+          <div class="copyright">
+            <div class="copyright__container">
+              <div class="grid">
+                <div class="row copyright__justify">
+                  <div class="col l-6">
+                    <div class="copyright__security">
+                      <div class="grid">
+                        <div class="row">
+                          <div class="col l-5">
+                            <p class="copyright__copyright">© 2021 Funio Furniture Store</p>
+                          </div>
+                          <div class="col l-7">
+                            <ul class="copyright__item">
+                              <li class="copyright__list">
+                                <a href="" class="copyright__link">PRIVACY</a>
+                              </li>
+                              <li class="copyright__list">
+                                <a href="" class="copyright__link">TERMS</a>
+                              </li>
+                              <li class="copyright__list">
+                                <a href="" class="copyright__link">*PROMO T&CS APPLY (VIEW HERE)</a>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col l-6">
+                    <div class="copyright_pay">
+                      <img src="./assets/img/pay.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+</div>
+<div class=" modal js-modal ">
+  <div class="modal-responsive modal-container js-modal-container">
+      <div class="modal-content">
+      <header class="modal-header">
+      <div class="modal-products-details">
+        <div class="modal-products-image">
+          <img src="" alt="">
+        </div>
+        <div class="modal-products-infomation">
+          <span class="modal-products-name">Alvarado Slideboard</span>
+          <span class="modal-products-quantity">Quantity: 1</span>
+          <span class="modal-products-price">$180.00</span>
+        </div>
+      </div>
+    </header>
+    <div class="modal-products-total">
+      <p class="modal-products-total-heading">Total</p>
+      <p class="modal-products-total-numbers">$180.00</p>
+    </div>
+    <div class="modal-product-cart">
+      <a href="" class="modal-product-cart-check ">VIEW CART</a>
+      <a href="" class="modal-product-cart-check checkout">CHECK OUT</a>
+    </div>
+    <footer class="modal-footer">
+      <span>FREE SHIPPING ON ALL <a href="">ODER OVER $75$</a></span>
+    </footer>
+      </div>
+   </div>      
+</div>
---
+++ .history/includes/nav_20211118183553.php
@@ -0,0 +1,112 @@
+<header class="header">
+            <div class="header__container">
+              <div class="header__topbar">
+                  <div class="header__topbar-left">
+                      <a href="" class="header__location">
+                          <i class="header__icon ti-location-pin"></i>
+                          <span class="header__text">Store Location</span>
+                      </a>
+                      <a href="" class="header__mail">
+                          <i class="header__icon-mail ti-email"></i>
+                          <span class="header__text">hau_1951220012@dau.edu.vn</span>
+                      </a>
+                  </div>
+                <div class="header__topbar-right">
+                      <a href="" class="header__text_gift">Gift Cards</a>
+                      <a href="" class="header__text">FAQs</a>
+                </div>
+              </div>
+              <div class="header__navigation">
+                <div class="header__menu">
+                  <div class="header__logo">
+                    <a href="./index.php" class="header__logo-link">
+                      <img src="./assets/img/logo.png" alt="" class="header__logo-img">
+                    </a>
+                  </div>
+                  <div class="header__nav">
+                    <ul class="header__nav-items">
+                      <li class="header__nav-lists">
+                          <a href="./index.php" class="header__nav-link active">HOME</a>                 
+                      </li>
+                      <li class="header__nav-lists">
+                          <a href="./shop_paging.php" class="header__nav-link">SHOP</a>
+                      </li>             
+                      <li class="header__nav-lists">
+                          <a href="" class="header__nav-link">PRODUCT</a>
+                      </li>
+                      <li class="header__nav-lists">
+                          <a href="./cart.php" class="header__nav-link">CART</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="headerr__active">
+                  <ul class="headerr__active-item">
+                    <li class="headerr__active-list">
+                      <a href="#" class="headerr__active-link">
+                        <i class="header__active-icon fas fa-search"></i>
+                      </a>
+                    </li>
+                    <li class="headerr__active-list header__navbar-user">
+                            <ul class="header__navbar-user-menu">
+                                <li class="header__navbar-user-item">
+                                    <img src="./assets/img/user-avt-deafault.png" alt="" width="30px" height="30px" class="user-avt-deafault">
+                                    <span class="username_login">
+                                    <?php
+                                        if (isset($_SESSION["login"])){
+                                        echo $_SESSION["login"][5];}
+                                    ?>
+                                    </span>
+                                </li>                       
+                                <li class="header__navbar-user-item">
+                                    <a href="">Tài Khoản của tôi</a>
+                                </li>
+                                <li class="header__navbar-user-item">
+                                    <a href="">Đơn mua </a>
+                                </li>
+                                <li class="header__navbar-user-item header__navbar-user-item--separate">
+                                  <div div class="user_logout"> 
+                                    <a href="logout.php" onclick=handleLogout()>Đăng xuất</a>                        
+                                </div>                                  
+                                </li>
+                          </ul>
+}
+                          <a href="login.php" class="headerr__active-link">
+                            <i class=" header__active-icon user-login far fa-user"></i>
+                          </a>                        
+                        <script>
+                          let getHeaderNav = document.querySelector('.header__navbar-user')
+                          // let getIconLogin = document.querySelector('.user-login')
+                          // let getAvtUser = document.querySelector('.user-avt-deafault')
+                          // let getBtnLogout = document.querySelector('.user_logout')     
+                          // let listIcon = document.querySelectorAll('.headerr__active-list')
+                          // function handleLogout(){
+                          //   getIconLogin.style.display = 'block';
+                          //   getAvtUser.style.display = 'none';
+                          //   getBtnLogout.style.display='none';
+                          //   listIcon[1].innerHTML = "<a href='login.php'><i class='header__active-icon user-login far fa-user'></i></a>";
+                          // }
+                          // if (getUserName.innerText != ''){
+                          //   getIconLogin.style.display = 'none';
+                          //   getAvtUser.style.display = 'block';
+                          //   getBtnLogout.style.display='block';
+                          // }
+                          getHeaderNav.setAttribute('style' , '.header__navbar-user:hover .header__navbar-user-menu{display: block ;}')
+                        </script>  
+                 
+                    </li>
+                    <li class="headerr__active-list">
+                      <a href="#" class="headerr__active-link">
+                        <i class="header__active-icon far fa-star"></i>
+                      </a>
+                    </li>
+                    <li class="headerr__active-list">
+                      <a href="#" class="headerr__active-link">
+                        <i class="header__active-icon fas fa-shopping-cart"></i>
+                      </a>
+                    </li>
+                  </ul>                 
+                </div>
+              </div>
+            </div>
+</header>
